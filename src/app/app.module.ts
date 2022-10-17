71df2ab386468c5f1304e8e71ca8849f7de1cb7e
Mahesh <maheshkshirsagar128@gmail.com>
2022-10-18 00:28:15 +0530

    Only Register and Login using their type and auth guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AllUserComponent } from './all-user/all-user.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    AllUserComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
